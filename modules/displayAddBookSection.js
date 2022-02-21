498107b3e43fa3dde7bb3a030e1a3d034ef4e0b2
Lucas Shangrita <oustanilokman@gmail.com>
2022-02-21 16:20:02 +0300

    fix linter errors

--- modules/displayAddBookSection.js
+++
@@ -1,6 +0,0 @@
-function displayAddBookSection(bookListSection, addBookSection, contactSection) {
-  bookListSection.classList.add('vanish');
-  addBookSection.classList.remove('vanish');
-  contactSection.classList.add('vanish');
-}
-export default displayAddBookSection;
--- modules/displayContactSection.js
+++
@@ -1,6 +0,0 @@
-function displayContactSection(bookListSection, addBookSection, contactSection) {
-  bookListSection.classList.add('vanish');
-  addBookSection.classList.add('vanish');
-  contactSection.classList.remove('vanish');
-}
-export default displayContactSection;
